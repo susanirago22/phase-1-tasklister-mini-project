3aff2dc4c91ef84f26d401220ce2961d4070012c
susanirago22 <susaniragoorani@gmail.com>
2022-12-06 13:46:37 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.getElementById('create-task-form')
+    form.addEventListener('submit', (e)=>{
+      e.preventDefault()
+      const data = e.target.new.value
+      const list = document.createElement('li')
+      list.textContent = data
+      const addTodo = document.getElementById('tasks')
+      const button = document.createElement('button')
+      button.textContent = 'x'
+      addTodo.appendChild(list)
+      addTodo.appendChild(button)
+      button.addEventListener('click', ()=>{
+        list.remove()
+        button.remove()
+      })
+    })
+  
 });
